af8d7fbb34781c101ed4720aae83ab96cc274868
Sahil <sahilkumarpcm@gmail.com>
2021-01-22 23:00:49 +0530

    post layout div

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 import { Post } from "../types";
 export default function Home() {
@@ -32,7 +33,25 @@ export default function Home() {
               </div>
               {/* {post data section} */}
               <div className="w-full p-2">
-                <p>{post.body}</p>
+                <div className="flex items-center">
+                  <Link href={`r/${post.subName}`}>
+                    <img
+                      src="https://www.lakshyaskills.com/wp-content/uploads/2018/08/default-gravatar.png"
+                      className="w-6 h-6 mr-1 rounded-full cursor-pointer"
+                    />
+                  </Link>
+                  <Link href={`r/${post.subName}`}>
+                    <a className="text-xs font-bold cursor-pointer hover:underline">
+                      /r/{post.subName}
+                    </a>
+                  </Link>
+                  <p className="text-xs text-gray-500">
+                    <span className="mx-1">•</span> Posted by
+                    <Link href={`/u/user`}>
+                      <a className="mx-1 hover:underline">/u/user</a>
+                    </Link>
+                  </p>
+                </div>
               </div>
             </div>
           ))}
